d662776614eb1b18164911300e332e781a552277
Kadir Günay <kadir.gunay@dygdigital.com>
2021-04-15 00:32:13 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,5 @@
-const path = require('path')
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-})
-
-const nextConfig = {
-    webpack: (config, options) => {
-        config.resolve.modules.push(path.resolve("./"));
-        return config;
-    },
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-    },
+module.exports = {
     images: {
         domains: ['images.ctfassets.net'],
     },
 }
-
-module.exports = withBundleAnalyzer(nextConfig)
